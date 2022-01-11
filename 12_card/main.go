2963f909b673164fece61a59de7c842aa132a057
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2022-01-11 21:45:10 +0600

    card experiment

--- 12_card/main.go
+++ 12_card/main.go
@@ -5,6 +5,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -17,15 +18,27 @@ func main() {
 
 	//card
 
-	card := widget.NewCard(
+	card1 := widget.NewCard(
 		"Md. Tareq Ibna Rahman",
 		"BDS, PGT, Leturer, PDC",
 		canvas.NewImageFromFile("./card_img.png"),
 	)
+	btn1 := widget.NewButton("Click", func() {
+
+	})
+	label1 := widget.NewLabel("This is test")
+	label2 := widget.NewLabel("This is test2")
 
 	// setup content
 	w.SetContent(
-		card,
+		container.NewHBox(
+			card1,
+			container.NewVBox(
+				label1,
+				label2,
+				btn1,
+			),
+		),
 	)
 	// show and run
 	w.ShowAndRun()
